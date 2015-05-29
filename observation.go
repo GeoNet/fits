653bd481a8f26cabb2acf319ab396909ffe2734c
bpeng <b.peng@gns.cri.nz>
2015-05-29 16:33:09 +1200

    add query for observation stats as required in issue #39

--- observation.go
+++ observation.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bytes"
 	"database/sql"
+	"encoding/json"
 	"github.com/GeoNet/web"
 	"github.com/GeoNet/web/api/apidoc"
 	"html/template"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 var eol []byte
@@ -21,6 +23,13 @@ var observationDoc = apidoc.Endpoint{Title: "Observation",
 	Queries: []*apidoc.Query{
 		observationD,
 		spatialObsD,
+	},
+}
+
+var observationStatsDoc = apidoc.Endpoint{Title: "Observation Statistics",
+	Description: `Get observation statistics.`,
+	Queries: []*apidoc.Query{
+		observationStatsD,
 	},
 }
 
@@ -44,6 +53,33 @@ var observationD = &apidoc.Query{
 		"column 1": obsDTDoc,
 		"column 2": obsValDoc,
 		"column 3": obsErrDoc,
+	},
+}
+
+var observationStatsD = &apidoc.Query{
+	Accept:      web.V1JSON,
+	Title:       "Observation Statistics",
+	Description: "Observations statisctics as JSON",
+	Example:     "/observation/stats?typeID=e&siteID=HOLD&networkID=CG",
+	ExampleHost: exHost,
+	URI:         "/observation/stats?typeID=(typeID)&siteID=(siteID)&networkID=(networkID)&[days=int]&[methodID=(methodID)]",
+	Required: map[string]template.HTML{
+		"typeID":    typeIDDoc,
+		"siteID":    siteIDDoc,
+		"networkID": networkIDDoc,
+	},
+	Optional: map[string]template.HTML{
+		"days":     `The number of days of data to select before now e.g., <code>250</code>.  Maximum value is 365000.`,
+		"methodID": methodIDDoc + `  typeID must be specified as well.`,
+	},
+	Props: map[string]template.HTML{
+		"Minimum":          obsMinDoc,
+		"maximum":          obsMaxDoc,
+		"First":            obsFirstDoc,
+		"Last":             obsLastDoc,
+		"Mean":             obsMeanDoc,
+		"StddevPopulation": obsPstdDoc,
+		"Unit":             obsUnitDoc,
 	},
 }
 
@@ -183,4 +219,122 @@ func observation(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", web.V1CSV)
 	web.OkBuf(w, r, &b)
+}
+
+/**
+ * query end point for observation statistics including: max, min, mean, std, first and last values
+ * http://fits.geonet.org.nz/observation/stats?typeID=e&siteID=HOLD&networkID=CG&days=100
+ */
+func observationStats(w http.ResponseWriter, r *http.Request) {
+	//1. check query parameters
+	if err := observationD.CheckParams(r.URL.Query()); err != nil {
+		web.BadRequest(w, r, err.Error())
+		return
+	}
+
+	v := r.URL.Query()
+
+	typeID := v.Get("typeID")
+	networkID := v.Get("networkID")
+	siteID := v.Get("siteID")
+
+	if !validType(w, r, typeID) {
+		return
+	}
+
+	var days int
+	var err error
+	var tmin, tmax time.Time
+
+	if v.Get("days") != "" {
+		days, err = strconv.Atoi(v.Get("days"))
+		if err != nil || days > 365000 {
+			web.BadRequest(w, r, "Invalid days query param.")
+			return
+		}
+		tmax = time.Now().UTC()
+		tmin = tmax.Add(time.Duration(days*-1) * time.Hour * 24)
+	}
+
+	var methodID string
+
+	if v.Get("methodID") != "" {
+		methodID = v.Get("methodID")
+		if !validTypeMethod(w, r, typeID, methodID) {
+			return
+		}
+	}
+
+	//2. Find the unit
+	var unit string
+	err = db.QueryRow("select symbol FROM fits.type join fits.unit using (unitPK) where typeID = $1", typeID).Scan(&unit)
+	if err == sql.ErrNoRows {
+		web.NotFound(w, r, "unit not found for typeID: "+typeID)
+		return
+	}
+
+	if err != nil {
+		web.ServiceUnavailable(w, r, err)
+		return
+	}
+
+	// retrieve values using existing functions
+	values, err := loadObs(networkID, siteID, typeID, methodID, tmin)
+	if err != nil {
+		web.ServiceUnavailable(w, r, err)
+		return
+	}
+
+	mean, stdDev, err := stddevPop(networkID, siteID, typeID, methodID, tmin)
+	if err != nil {
+		web.ServiceUnavailable(w, r, err)
+		return
+	}
+	stats := Obstats{Unit: unit,
+		Mean:             mean,
+		StddevPopulation: stdDev}
+
+	//4. get maximum, minimum, first, last values
+	firstVal := values[0]
+	lastVal := values[len(values)-1]
+	stats.First = ObsValue{DateTime: firstVal.T.Format(time.RFC3339),
+		Value: firstVal.V,
+		Error: firstVal.E}
+
+	stats.Last = ObsValue{DateTime: lastVal.T.Format(time.RFC3339),
+		Value: lastVal.V,
+		Error: lastVal.E}
+
+	iMin, iMax, _ := extremes(values)
+	minVal := values[iMin]
+	maxVal := values[iMax]
+
+	stats.Minimum = ObsValue{DateTime: minVal.T.Format(time.RFC3339),
+		Value: minVal.V,
+		Error: minVal.E}
+
+	stats.Maximum = ObsValue{DateTime: maxVal.T.Format(time.RFC3339),
+		Value: maxVal.V,
+		Error: maxVal.E}
+
+	//5. send result response
+	w.Header().Set("Content-Type", web.V1JSON)
+	b, _ := json.Marshal(stats)
+	web.Ok(w, r, &b)
+}
+
+type Obstats struct {
+	Maximum          ObsValue
+	Minimum          ObsValue
+	First            ObsValue
+	Last             ObsValue
+	Mean             float64
+	StddevPopulation float64
+	Unit             string
+}
+
+type ObsValue struct {
+	DateTime string
+	Value    float64
+	Error    float64
 }
