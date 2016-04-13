3d34f0ca2cd5865319515d3f70507fca3f68b047
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2016-04-13 21:58:01 +1200

    Update vendored deps (log/logentries)

--- vendor/github.com/GeoNet/log/logentries/logentries.go
+++ vendor/github.com/GeoNet/log/logentries/logentries.go
@@ -79,22 +79,43 @@ type sender struct {
 type Writer struct {
 }
 
+type Blocker struct {
+}
+
+// Optionally set the log prefix at compilation e.g.,
+// go build ... -ldflags "-X github.com/GeoNet/log/logentries.Prefix=string"
+var Prefix string
+
 var s sender
-var w Writer
+
 var le chan string
 
 var std = os.Stderr
+var once sync.Once
 
-// Init reconfigures the logger to send to Logentries using TLS.
-// The send to Logentries is via a buffered chan and
-// messages will not be sent to Logentries if the chan is full.
-// Calls with an empty token no-op.
-func Init(token string) {
-	if token == "" {
-		log.Println("Not sending to Logentries - empty token.")
-		return
+func init() {
+	if Prefix != "" {
+		log.SetPrefix(Prefix + " ")
 	}
 
+	token := os.Getenv("LOGENTRIES_TOKEN")
+
+	if token != "" {
+		once.Do(func() { initLogentries(token) })
+	}
+}
+
+// Init reconfigures the logger to send to Logentries using TLS.
+// The default behaviour sends to Logentries is via a buffered chan and
+// messages will not be sent to Logentries if the chan is full.
+// To block on write to Logentries set the env var LOGENTRIES_BLOCKING=true
+//
+// Calls with an empty LOGENTRIES_TOKEN no-op.
+func Init(token string) {
+	once.Do(func() { initLogentries(token) })
+}
+
+func initLogentries(token string) {
 	log.Println("Logging to Logentries")
 
 	s = sender{token: token + " "}
@@ -105,35 +126,47 @@ func Init(token string) {
 		return
 	}
 
-	w = Writer{}
-
-	log.SetOutput(w)
-
 	le = make(chan string, 100)
 
-	go func() {
-		defer s.conn.Close()
-		for {
-			select {
-			case m := <-le:
-				if _, err := writeAndRetry(m); err != nil {
-					std.Write([]byte(fmt.Sprintf("WARN sending to Logentries: %s\n", err)))
+	switch os.Getenv("LOGENTRIES_BLOCKING") {
+	case "true":
+		log.SetOutput(Blocker{})
+	default:
+		log.SetOutput(Writer{})
+		go func() {
+			defer s.conn.Close()
+			for {
+				select {
+				case m := <-le:
+					if _, err := writeAndRetry(m); err != nil {
+						std.Write([]byte(fmt.Sprintf("WARN sending to Logentries: %s\n", err)))
+					}
 				}
 			}
-		}
-	}()
+		}()
+	}
 }
 
 // Write writes to Logentries using TLS via a buffered chan.  If the chan if full then messages are
 // not saved for sending  to Logentries.
 func (w Writer) Write(b []byte) (int, error) {
-	if len(le) < cap(le) {
-		le <- string(b)
-	} else {
-		std.Write([]byte("WARN Logentries buffer full."))
-		return std.Write(b)
+	select {
+	case le <- string(b):
+	default:
+		std.Write(b)
 	}
+
 	return len(b), nil
+}
+
+// Write writes to Logentries over TLS.  Blocks till the write succeeds or tries twice and fails.
+func (bl Blocker) Write(b []byte) (int, error) {
+	i, err := writeAndRetry(string(b))
+	if err != nil {
+		std.Write([]byte(fmt.Sprintf("WARN sending to Logentries: %s - %s\n", err, string(b))))
+	}
+
+	return i, err
 }
 
 // connect makes a TLS connection to Librato.  It must be called with s.mu held.
