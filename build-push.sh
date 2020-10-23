fc798298c01ee61cd2e7c74ceec12a5bc9805b5c
bpeng <b.peng@gns.cri.nz>
2020-10-23 13:42:06 +1300

    fix docker build error

--- build-push.sh
+++ build-push.sh
@@ -8,14 +8,15 @@ ACCOUNT=$(aws sts get-caller-identity --output text --query 'Account')
 VERSION='git-'`git rev-parse --short HEAD`
 
 for i in "$@"
+
 do
   if [[ "${i}" == "fits-api" ]]; then
-    ./build.sh $@
+    ./build.sh ${i}
 	docker push ${ACCOUNT}.dkr.ecr.ap-southeast-2.amazonaws.com/${i}:$VERSION
 	docker push ${ACCOUNT}.dkr.ecr.ap-southeast-2.amazonaws.com/${i}:latest
   else
     cd dapper
-    ./dockerbuild.sh $@
+    ./dockerbuild.sh ${i}
     cd ..
   fi
 done
